3733dd9d943589eef31449fa148e085d192ea6d9
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-10-24 23:47:54 +0530

    header items li

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,31 +12,41 @@ function Header() {
             alt="dnd-logo"
           />
         </div>
-        <div className="montserrat-subrayada-bold hover:cursor-pointer ">
-          <ul className="flex justify-center items-center ">
+        <div className="montserrat-subrayada-bold hover:cursor-pointer  ">
+          <ul className="flex md:flex-row gap-8 justify-center items-center ">
             <li>
-              <a className="text-[#F55E04] px-2" href="/">
+              <a className="text-[#F55E04]" href="/">
                 Home
               </a>
-              <a className="hover:text-[#F55E04] px-2" href="/team">
+            </li>
+            <li>
+              <a className="hover:text-[#F55E04]" href="/team">
                 Team
               </a>
-              <a className="hover:text-[#F55E04] px-2" href="/about">
+            </li>
+            <li>
+              <a className="hover:text-[#F55E04]" href="/about">
                 About
               </a>
-              <a className="hover:text-[#F55E04] px-2" href="/events">
+            </li>
+            <li>
+              <a className="hover:text-[#F55E04]" href="/events">
                 Events
               </a>
-              <a className="hover:text-[#F55E04] px-2" href="/blog">
+            </li>
+            <li>
+              <a className="hover:text-[#F55E04]" href="/blog">
                 Blog
               </a>
-              <a className="hover:text-[#F55E04] px-2" href="/gallery">
+            </li>
+            <li>
+              <a className="hover:text-[#F55E04]" href="/gallery">
                 Gallery
               </a>
             </li>
           </ul>
         </div>
-        <div className="button montserrat-subrayada-bold">
+        <div className="montserrat-subrayada-bold ">
           <button className="bg-[#F55E04] text-white px-6 py-3 rounded hover:cursor-pointer  ">
             Contact Us
           </button>
