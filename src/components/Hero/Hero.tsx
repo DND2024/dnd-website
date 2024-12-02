9bfeec0f9cff711383c9f41a60294db557acf4f3
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-02 20:29:38 +0530

    hero responsive

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,35 +4,39 @@ import CardLatestBuzz from "../elements/CardLatestBuzz";
 function Hero() {
   return (
     <section className="hero">
-      <div className="bg-[#EFEEE0] flex flex-col md:flex-row border-t-2 border-black">
+      <div className="bg-[#EFEEE0] w-screen h-fit flex flex-col md:flex-row border-t-2 border-black">
         {/* <img className="w-3/6 h-3/6" src="/bg-image.webp" /> */}
-        <div className="flex flex-row items-center w-screen md:w-1/2 h-1/2 md:h-screen">
+        <div className="flex flex-row items-center w-screen md:w-1/2 h-screen ">
           <img
-            className="w-1/4 h-[20%] flex self-end"
+            className="w-1/4 h-[20%] flex self-end m-0 mb-[10%] md:ms-[10%]"
             src="/arrow-up.webp"
             alt="arrow"
           />
-          <div className="w-2/4 h-[40%]">
-            <h1 className="nothing-you-could-do-regular text-2xl text-[#F50004] ">
+          <div className="w-2/4 h-[40%] flex flex-col items-start">
+            <h1 className="flex nothing-you-could-do-regular text-2xl text-[#F50004] ">
               An NITC Club
             </h1>
             <br />
-            <h1 className="lexend-font text-9xl text-[#FF5900]	">DND</h1>
+            <h1 className="flex lexend-font text-9xl text-[#FF5900]">DND</h1>
             <br />
-            <h1 className="amaranth-regular text-2xl bg-gradient-to-r from-[#00A6FF] to-[#FF14CB] text-transparent bg-clip-text">
+            <h1 className="flex amaranth-regular text-2xl bg-gradient-to-r from-[#00A6FF] to-[#FF14CB] text-transparent bg-clip-text">
               Where drama creates magic <br />
               dance burns the floor!{" "}
             </h1>
           </div>
           <img
-            className="w-1/4 h-[20%] flex self-start"
+            className="w-1/4 h-[20%] flex self-start m-0 md:me-[10%] mt-[10%]"
             src="/arrow-down.webp"
             alt="arrow"
           />
         </div>
         <div className="flex md:h-screen w-screen md:w-1/2 items-center">
           {/* <img className="h-14 mt-[50%] " src="/dots.webp" alt="dance" /> */}
-          <img className="h-[80%] " src="/home-dance.webp" alt="dance" />
+          <img
+            className="h-[80%] md:mb-[10%] "
+            src="/home-dance.webp"
+            alt="dance"
+          />
         </div>
       </div>
     </section>
