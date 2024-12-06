3be1fcd61149f3b31aa221dd20854429d772fabf
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-06 23:02:19 +0530

    footer

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,15 +1,9 @@
-import Achievements from "@/elements/Achievements/Achievements";
-import CardLatestBuzz from "@/components/CardLatestBuzz";
-import CardVideo from "@/components/CardVideo";
-import Events from "@/elements/Events/Events";
 import React from "react";
 
 function page() {
   return (
     <div>
-      <div>
-        <Achievements />
-      </div>
+
     </div>
   );
 }
