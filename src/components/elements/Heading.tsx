0b4394dc02db53afb7a1f28d67e1841ca7860908
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-09 17:44:50 +0530

    heading bold

--- src/components/elements/Heading.tsx
+++ src/components/elements/Heading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Heading({ heading }: { heading: any }) {
   return (
     <div className="flex justify-center my-6 ">
-      <h1 className="ubuntu-light font-bold text-2xl border-b-4 w-fit border-black">
+      <h1 className="ubuntu-light font-extrabold text-2xl border-b-4 w-fit border-black">
         {heading.value}
       </h1>
     </div>
