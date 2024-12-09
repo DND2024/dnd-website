c2294b2e27ce6a73f6b4190db126e3feb886e2eb
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-09 19:28:02 +0530

    section created for theme change button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import Button from "../elements/Button";
 function Header() {
   return (
     <nav className="flex flex-row w-screen px-[2%] items-center border-b-2 border-black backdrop-blur-md fixed ">
-      <div className="w-1/2 md:w-[20%] flex justify-center ">
+      <div className="w-1/3 md:w-[20%] flex justify-center ">
         <img
           className="h-20 hover:cursor-pointer static"
           src="/dnd-logo.webp"
@@ -49,11 +49,14 @@ function Header() {
         </ul>
       </div>
 
-      <div className="w-1/2 md:w-[20%] flex justify-center">
-        <div className=" w-3/4 flex justify-center">
+      <div className="w-2/3 md:w-[20%] flex justify-between bg-red-500">
+        <div className="w-1/4 flex justify-center items-center bg-slate-500">
+        
+        </div>
+        <div className=" w-3/4 flex justify-center items-center bg-green-500">
           <Button text={{ value: "Contact Us" }} />
         </div>
-        <div className=" w-1/4 flex  md:hidden">
+        <div className=" w-1/4 flex justify-center items-center md:hidden bg-lime-500">
           <IoMenu className="text-5xl" />
         </div>
       </div>
