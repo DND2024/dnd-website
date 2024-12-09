25972f196a5a62bcafd3240e25f8db4d95d0c626
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-09 16:42:00 +0530

    elements added

---
+++ src/components/elements/CardLatestBuzz.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function CardLatestBuzz() {
+  return (
+    <div className="card my-10 md:w-2/6 md:mx-[4%] p-[6%] md:p-[2%] rounded-2xl bg-[#B9BCA4] ">
+      <img
+        className="card-img-top rounded-2xl"
+        src="https://www.visionvivaah.com/blog/wp-content/uploads/2019/12/Event-Organisers-For-Dance-Parties-1024x514.jpg"
+        alt="Card image cap"
+      />
+      <div className="card-body">
+        <h5 className="card-title py-3 font-extrabold text-xl ">Music Night</h5>
+        <p className="card-text font-light ">
+          Some quick example text to build on the card title and make up the
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default CardLatestBuzz;
