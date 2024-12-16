43632f200d73488c9d4045fd401108d356dd7e2f
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-17 00:49:55 +0530

    clear work page

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,14 +1,8 @@
-import Footer from "@/components/Footer/Footer";
-import Meet from "@/components/Meetmembers/Meet";
-import TalkToUs from "@/components/TalkToUs/TalkToUs";
 import React from "react";
 
 function page() {
   return (
     <div>
-      <TalkToUs/>
-      <Meet/>
-      <Footer/>
     </div>
   );
 }
