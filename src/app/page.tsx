e23a15b20fc3de69e3541864ede83386945ff738
Amitk7771 <amittkumarr51@gmail.com>
2024-12-18 13:46:07 +0530

    improved Responsiveness all components and 'more' link redirected to team pg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import Achievements from "@/components/Achievements/Achievements";
 import DanceSection from "@/components/DaceSection/DanceSection";
 import Button from "@/components/elements/Button";
 import Events from "@/components/Events/Events";
+import Footer from "@/components/Footer/Footer";
 import Gallery from "@/components/Gallery/Gallery";
 import Header from "@/components/Header/Header";
 import Hero from "@/components/Hero/Hero";
 import Latest from "@/components/Latest/Latest";
+import Meet from "@/components/Meetmembers/Meet";
+import TalkToUs from "@/components/TalkToUs/TalkToUs";
 import Video from "@/components/Video/Video";
 import React from "react";
 
@@ -25,6 +28,12 @@ function page() {
       <Events />
       <Achievements />
       <DanceSection />
+      <TalkToUs/>
+      <Meet/>
+      <Footer/>
+
+
+     
     </div>
   );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,43 +3,42 @@ import "./footer.css";
 
 function Footer() {
   return (
+    
+    <footer className="footer bg-[url(/clip.webp)] bg-cover  dark:bg-[#161922] p-0.5 sm:p-4 mx-[1%] sm:mx-[2%] md:p-8">
+      <div className="footer-logo flex justify-center mb-0 sm:mb-4">
+        <img src="dnd-logo.webp" alt="DND Logo" className="dnd-logo w-24 md:w-32" />
+      </div>
 
-    <footer className="footer bg-[url(/clip.webp)] bg-cover flex-shrink overflow-hidden dark:bg-[#161922] p-4 md:p-8">
-      
-      <div className="footer-logo flex justify-center mb-4">
-        <img src="Mask group.webp" alt="DND Logo" className="dnd-logo w-24 md:w-32" />
+      <hr className="footer-line dark:border-gray-500  "  />
+
+      <div className="footer-nav flex flex-wrap sm:flex-nowrap justify-center gap-2 sm:gap-5 mb-0 sm:mb-4 dark:text-white w-[80%]">
+        <a href="/home" className="text-xs md:text-sm lg:text-base">Home</a>
+        <a href="/events" className="text-xs md:text-sm lg:text-base">Events</a>
+        <a href="/gallery" className="text-xs md:text-sm lg:text-base">Gallery</a>
+        <a href="/about" className="text-xs md:text-sm lg:text-base">About</a>
+        <a href="/blog" className="text-xs md:text-sm lg:text-base">Blog</a>
+        <a href="/team" className="text-xs md:text-sm lg:text-base">Our Team</a>
       </div>
 
       <hr className="footer-line dark:border-gray-500" />
 
-      <div className="footer-nav  w-[80%] flex flex-nowrap  justify-center gap-5 mb-4 dark:text-white overflow-x-hidden">
-        <a href="/home" className=" text-sm md:text-base shrink-2">Home</a>
-        <a href="/events" className="text-sm md:text-base shrink-2">Events</a>
-        <a href="/gallery" className="text-sm md:text-base shrink-2">Gallery</a>
-        <a href="/about" className="text-sm md:text-base shrink-2">About</a>
-        <a href="/blog" className="text-sm md:text-base shrink-2">Blog</a>
-        <a href="/team" className="text-sm md:text-base shrink-2">Our Team</a>
-      </div>
-
-      <hr className="footer-line dark:border-gray-500" />
-
-      <div className="footer-contact flex  md:flex-row justify-between w-full mb-4 pl-4 pr-4 md:pl-30 md:pr-37.5 dark:invert">
-        <div className="footer-location flex items-center gap-2.5 mb-4 md:mb-0">
-          <img src="location.webp" alt="Location" className="icon w-6 md:w-8" />
-          <p className="text-sm md:text-base">
+      <div className="footer-contact flex flex-col sm:flex-row  justify-between  w-full mb-4 sm:gap-5  sm:px-60 dark:invert">
+        <div className="footer-location flex items-center gap-2.5 mb-4 justify-center md:mb-0">
+          <img src="location.webp" alt="Location" className="icon max-w-full md:w-8" />
+          <p className="text-xs md:text-sm lg:text-base">
             DND CLUB
             <br />
-            NATIONAL INSTITUTE OF TECHNOLOGY CALICUT
+            NATIONAL INSTITUTE OF TECHNOLOG CALICUT
             <br />
             KOZHIKODE, KERALA
             <br />
             673601
           </p>
         </div>
-        <div className="footer-phone flex flex-col items-start gap-2.5 pl-4 md:pl-12.5">
-          <p className="contact-us text-sm md:text-base">CONTACT US</p>
+        <div className="footer-phone flex flex-col mx-auto sm:mx-0 gap-2.5">
+          <p className="contact-us text-xs md:text-sm lg:text-base">CONTACT US</p>
           <img src="contact_logo.webp" alt="Contact" className="icon phone-icon w-6 md:w-8" />
-          <p className="underline text-sm md:text-base">+91 987654321 &nbsp; +91 987654321</p>
+          <p className="underline text-xs md:text-sm lg:text-base">+91 987654321 &nbsp; +91 987654321</p>
         </div>
       </div>
 
@@ -57,7 +56,7 @@ function Footer() {
         </a>
       </div>
 
-      <div className="footer-image flex justify-center flex-shrink-0">
+      <div className="footer-image flex justify-center w-[95%] sm:w-full pb-[3%] sm:pb-0">
         <img src="dnd-footer.webp" alt="Dance and Drama Club" className="footer-image w-full md:w-3/4" />
       </div>
     </footer>
