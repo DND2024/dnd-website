d37c4a46cb85b1ea25366bd9a63c8e1524418cb8
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-18 19:48:22 +0530

    clear work directory

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,17 +1,8 @@
-import Footer from "@/components/Footer/Footer";
-import Header from "@/components/Header/Header";
-import Meet from "@/components/Meetmembers/Meet";
-import TalkToUs from "@/components/TalkToUs/TalkToUs";
 import React from "react";
 
 function page() {
   return (
-        <>
-        <TalkToUs/>
-        <Meet/>
-        <Footer/>
-        </>
-   
+    <div></div>
   );
 }
 
