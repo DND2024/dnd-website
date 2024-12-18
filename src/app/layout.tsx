30ee406949536534923fdb5d10ec39692c932853
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-18 19:32:46 +0530

    Joker font added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Amaranth&family=Island+Moments&family=Lexend&family=Montserrat+Subrayada&family=Nothing+You+Could+Do&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Amaranth&family=Island+Moments&family=Lexend&family=Montserrat+Subrayada&family=Kablammo&family=Nothing+You+Could+Do&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
           rel="stylesheet"
         />
         <script
