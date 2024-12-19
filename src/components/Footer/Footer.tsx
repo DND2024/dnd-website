e951766f71ea07c1f4e01e18ef52f7034e89dd57
Amitk7771 <amittkumarr51@gmail.com>
2024-12-19 11:26:05 +0530

    footer improvements done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import "./footer.css";
 function Footer() {
   return (
     
-    <footer className="footer bg-[url(/clip.webp)] bg-cover  dark:bg-[#161922] p-0.5 sm:p-4 mx-[1%] sm:mx-[2%] md:p-8">
+    <footer className="footer bg-[url(/clip.webp)] bg-cover  dark:bg-[#161922] p-x-[25%] sm:p-x-[10%] m-[2%] sm:m-x-[5%] md:p-x-[20%]">
       <div className="footer-logo flex justify-center mb-0 sm:mb-4">
         <img src="dnd-logo.webp" alt="DND Logo" className="dnd-logo w-24 md:w-32" />
       </div>
@@ -22,23 +22,30 @@ function Footer() {
 
       <hr className="footer-line dark:border-gray-500" />
 
-      <div className="footer-contact flex flex-col sm:flex-row  justify-between  w-full mb-4 sm:gap-5  sm:px-60 dark:invert">
-        <div className="footer-location flex items-center gap-2.5 mb-4 justify-center md:mb-0">
+      <div className="footer-contact flex flex-col sm:flex-row  justify-between  w-full mb-4 sm:gap-10  sm:px-[20%] dark:invert">
+        
+        <div className="footer-location pr-[5%] flex items-center gap-2.5 mb-4 justify-center md:mb-0">
           <img src="location.webp" alt="Location" className="icon max-w-full md:w-8" />
           <p className="text-xs md:text-sm lg:text-base">
             DND CLUB
             <br />
-            NATIONAL INSTITUTE OF TECHNOLOG CALICUT
+            NATIONAL INSTITUTE OF <br/> TECHNOLOGY CALICUT
             <br />
             KOZHIKODE, KERALA
             <br />
             673601
           </p>
         </div>
-        <div className="footer-phone flex flex-col mx-auto sm:mx-0 gap-2.5">
-          <p className="contact-us text-xs md:text-sm lg:text-base">CONTACT US</p>
-          <img src="contact_logo.webp" alt="Contact" className="icon phone-icon w-6 md:w-8" />
-          <p className="underline text-xs md:text-sm lg:text-base">+91 987654321 &nbsp; +91 987654321</p>
+        
+        <div className="footer-phone  flex flex-col justify-center items-center sm:items-baseline sm:mx-0 gap-2.5">
+          <p className="contact-us sm:pl-[14%] md:pl-[3] lg:pl-[0.5] text-xs md:text-sm lg:text-base">CONTACT US</p>
+          <img src="contact_logo.webp" alt="Contact" className=" pl-[1%] lg:-pl-[0.5] icon phone-icon w-6 md:w-8" />
+          {/* <p className=" sm:pl-[14%] md:pl-[3] lg:-pl-[0.5]text-xs text-center sm:text-start md:text-sm lg:text-base whitespace-nowrap">+91 9876543210 &nbsp; +91 9876543210</p> */}
+          <div className="sm:flex  sm:pl-[14%] md:pl-[3] lg:-pl-[0.5]text-xs text-center sm:text-start md:text-sm lg:text-base">
+            <span  className=" sm:whitespace-nowrap" >+91 9876543210</span>
+            <span className=" whitespace-nowrap"> &nbsp; +91 9876543210</span>
+          </div>
+        
         </div>
       </div>
 
