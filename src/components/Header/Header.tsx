dee833f4091417eb87acf17205464a25a492e845
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-20 16:55:31 +0530

    header responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,15 +6,15 @@ import ThemeSwitcher from "../elements/ThemeSwitcher";
 
 function Header() {
   return (
-    <nav className="z-20 flex flex-row w-screen px-[2%] items-center border-b-2 border-black backdrop-blur-md fixed ">
-      <div className="w-1/3 md:w-[20%] flex justify-center ">
+    <nav className="z-20 flex flex-row w-screen px-[2%] justify-between items-center border-b-2 border-black backdrop-blur-md fixed ">
+      <div className="w-fit lg:w-[20%] flex justify-start bg-lime-200">
         <img
           className="h-20 hover:cursor-pointer static"
           src="/dnd-logo.webp"
           alt="dnd-logo"
         />
       </div>
-      <div className="w-0 md:w-[60%] md:flex justify-center hidden ">
+      <div className="w-0 lg:w-[60%] lg:flex justify-center hidden ">
         <ul className="flex flex-row montserrat-subrayada hover:cursor-pointer gap-6 text-black dark:text-white">
           <li>
             <a className="hover:text-[#F55E04]" href="/">
@@ -49,16 +49,16 @@ function Header() {
         </ul>
       </div>
 
-      <div className="w-2/3 md:w-[20%] flex justify-between ">
-        <div id="theme" className="w-1/4 flex justify-center items-center">
+      <div className="w-[80%] lg:w-[20%] justify-end gap-3 flex bg-slate-400">
+        <div className="w-fit flex justify-center items-center bg-red-300">
           <div className="w-10 h-10 border-2 border-black dark:border-white rounded-md justify-items-center items-center p-2 hover:cursor-pointer">
             <ThemeSwitcher />
           </div>
         </div>
-        <div className=" w-3/4 flex justify-center items-center">
+        <div className=" w-fit flex justify-center items-center bg-amber-300">
           <Button text={{ value: "Contact Us" }} />
         </div>
-        <div className=" w-1/4 flex justify-center items-center md:hidden">
+        <div className=" w-fit flex justify-center items-center lg:hidden bg-lime-300">
           <IoMenu className="text-5xl" />
         </div>
       </div>
--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({ text }: { text: any }) {
   return (
     <div>
-      <button className="bg-[#F55E04] text-white px-6 py-3  rounded hover:cursor-pointer  montserrat-subrayada">
+      <button className="bg-[#F55E04] text-sm  md:text-lg  text-white px-2 md:px-6 py-3  rounded hover:cursor-pointer  montserrat-subrayada">
         {text.value}
       </button>
     </div>
