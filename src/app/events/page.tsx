3a9e756d763fada8a6bfdaf41022ffc52a145d10
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-20 11:44:52 +0530

    event page completed

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,12 +1,14 @@
 import EventsHero from "@/components/EventsHero/EventsHero";
+import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import React from "react";
 
 function page() {
   return (
-    <div className="flex flex-col overflow-hidden">
+    <div className="flex flex-col bg-[#EFEEE0] dark:bg-[#110F0F] overflow-x-hidden">
       <Header />
       <EventsHero />
+      <Footer />
     </div>
   );
 }
